8774c14f844f733eb27358a19cc1b3206a7f1053
Yuchen Li <liyuchen0615@hotmail.com>
2018-01-22 16:20:56 -0500

    Soysauce

--- NLP_practice.py
+++ NLP_practice.py
@@ -9,3 +9,38 @@ Created on Thu Jan 18 16:20:26 2018
 
 from nltk.book import *
 from nltk import *
+
+
+"""
+http://www.nltk.org/book/ch01.html#fig-inaugural
+"""
+text1
+
+text1.concordance("china")
+
+text1.similar("china")
+text2.similar("china")
+
+text2.concordance("monstrous")
+
+text4.dispersion_plot(["citizens", "democracy", "freedom", "duties", "America"])
+
+text3.generate()
+
+
+"""
+1.4 Counting Vocabulary
+"""
+len(text3)
+
+sorted(set(text3))
+
+text5.count("lol")
+
+
+"""
+3.1 Frequency Distribution
+"""
+fdist1 = FreqDist(text1)
+print(fdist1)
+fdist1.most_common(20)
