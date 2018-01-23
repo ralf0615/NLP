e32974a3ad8962a8e12c81ecc8211fcfa8973df2
Yuchen Li <liyuchen0615@hotmail.com>
2018-01-23 16:40:40 -0500

    Soysauce

--- NLP_practice.py
+++ NLP_practice.py
@@ -9,6 +9,7 @@ Created on Thu Jan 18 16:20:26 2018
 
 from nltk.book import *
 from nltk import *
+import nltk as nltk
 
 
 """
@@ -37,10 +38,32 @@ sorted(set(text3))
 
 text5.count("lol")
 
-
 """
 3.1 Frequency Distribution
 """
 fdist1 = FreqDist(text1)
 print(fdist1)
 fdist1.most_common(20)
+
+
+"""
+3.3 Collocation and Bigrams
+Collocation is a sequence of words that occur together unusually often
+"""
+
+"""
+5.5 Spoken Dialog Systems
+"""
+# nltk.chat.chatbots() error
+
+"""
+2. Accesing Text Corpora and Lexical Resources
+"""
+
+import nltk
+nltk.corpus.gutenberg.fileids()
+
+"""
+1.8 Text Corpus Structure
+"""
+sents = gutenberg.sents("burgess-busterbrown.txt")
